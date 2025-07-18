ac53d86a22ee03fc523ac62a28576410bb958c6f
ruth-chirwa <chirwaruth647@gmail.com>
2025-07-18 10:46:01 +0200

    add to dist folder

--- dist/index.js
+++ dist/index.js
@@ -25,8 +25,18 @@ class TaskManager {
         const task = this.getTaskById(id);
         if (task) {
             task.completed = true;
-            console.log(`${task.title} is ${task.completed ? "complete" : "incomplete"}`);
+            console.log(`${task.title} mark as complete`);
         }
+        else {
+            console.log(`Task ${id} not found`);
+        }
+    }
+    listAllTasks() {
+        console.log(`\n All Tasks`);
+        this.tasks.forEach(task => {
+            const status = task.completed ? "complete" : "incomplete";
+            console.log(`${task.id}: ${task.title} - ${status}`);
+        });
     }
 }
 // === run ===
@@ -35,6 +45,12 @@ console.log(`\n Added Tasks`);
 manage.addTask(new Task(1, "Write", "Write reports"));
 manage.addTask(new Task(2, "Clean", "Do laundry"));
 manage.addTask(new Task(3, "Practice", "Practice coding for 1 hour"));
+// list all tasks before completion
+manage.listAllTasks();
 console.log(`\n Completed Tasks`);
 manage.markTaskComplete(2);
 manage.markTaskComplete(3);
+console.log(`\n Error messages`);
+manage.markTaskComplete(4);
+// list all tasks after marking completion
+manage.listAllTasks();
